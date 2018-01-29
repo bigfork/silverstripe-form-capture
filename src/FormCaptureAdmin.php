d7dba4091f43af9269973e113a829258daae4682
Andrew Haine <andrewhaineweb@gmail.com>
2018-01-29 19:08:19 +0000

    Remove standard modeladmin buttons

--- src/FormCaptureAdmin.php
+++ src/FormCaptureAdmin.php
@@ -3,6 +3,10 @@
 namespace SSFormCapture;
 
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\GridField\GridFieldImportButton;
+use SilverStripe\Forms\GridField\GridFieldPrintButton;
+
 use SSFormCapture\CapturedFormSubmission;
 
 class MyAdmin extends ModelAdmin
@@ -15,5 +19,15 @@ class MyAdmin extends ModelAdmin
     private static $menu_title = 'Form Submissions';
 
     private static $menu_icon ='silverstripe-form-capture/icon/captured-form-submissions.png';
+
+    public function getEditForm($id = null, $fields = null)
+	{
+		$form = parent::getEditForm();
+		$gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
+		$gridField->getConfig()->removeComponentsByType(GridFieldExportButton::class);
+		$gridField->getConfig()->removeComponentsByType(GridFieldImportButton::class);
+		$gridField->getConfig()->removeComponentsByType(GridFieldPrintButton::class);
+		return $form;
+	}
 
 }
