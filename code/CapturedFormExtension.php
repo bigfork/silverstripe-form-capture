0d5c7a6605dc6aa3f112dd86c261fe03d45d9bd7
Andrew Haine <andrewhaine@bigfork.co.uk>
2018-04-19 11:46:47 +0100

    [SS3] Add return values when capturing forms
    Provides some useful information which can be used in the controller

--- code/CapturedFormExtension.php
+++ code/CapturedFormExtension.php
@@ -2,85 +2,129 @@
 
 class CapturedFormExtension extends Extension
 {
-	/**
-	 * Add a method to Form which will capture data when invoked
-	 * @param string $dataName Am optional name for the submission
-	 * @param mixed $excludedFields An array or string of fields to be ignored when saving the submission
-	 * @param mixed $inDetails Fields to be included in the 'details' column in the admin area
-	 * @return null
-	 */
-	public function captureForm($dataName = 'Form Submission', $excludedFields = [], $inDetails = []) {
+
+    /**
+    * Add a method to Form which will capture data when invoked
+    * @param string $dataName Am optional name for the submission
+    * @param mixed $excludedFields An array or string of fields to be ignored when saving the submission
+    * @param mixed $inDetails Fields to be included in the 'details' column in the admin area
+    * @return null
+    */
+    public function captureForm($dataName = 'Form Submission', $excludedFields = [], $inDetails = [])
+    {
 
         $form = $this->owner;
 
-		// Create a blank form submission and write to database so that we have an ID to work with
-		$submission = CapturedFormSubmission::create();
-		$submission->Type = $dataName;
-		$submission->write();
+        // Create a blank form submission and write to database so that we have an ID to work with
+        $submission = CapturedFormSubmission::create();
+        $submission->Type = $dataName;
+        $submission->write();
 
-		// Grab all the fields
-		$fieldsToWrite = $form->fields->dataFields();
+        // Grab all the fields
+        $fieldsToWrite = $form->fields->dataFields();
 
-		// Allow the excluded fields and details fields to be a single string
-		$excludedFields = is_array($excludedFields) ? $excludedFields : [$excludedFields];
-		$inDetails = is_array($inDetails) ? $inDetails : [$inDetails];
+        // Allow the excluded fields and details fields to be a single string
+        $excludedFields = is_array($excludedFields) ? $excludedFields : [$excludedFields];
+        $inDetails = is_array($inDetails) ? $inDetails : [$inDetails];
 
-		// Ignore SecurityID by default
-		array_push($excludedFields, 'SecurityID');
+        // Ignore SecurityID by default
+        array_push($excludedFields, 'SecurityID');
 
-		// Remove any unwanted fields from the fields array
-		foreach($excludedFields as $excludedField) {
-			if($form->fields->dataFieldByName($excludedField)) {
-				unset($fieldsToWrite[$excludedField]);
-			};
-		}
+        // Remove any unwanted fields from the fields array
+        foreach($excludedFields as $excludedField) {
+            if($form->fields->dataFieldByName($excludedField)) {
+                unset($fieldsToWrite[$excludedField]);
+            };
+        }
 
-		// For every wanted field create a Captured Field object and write it to this submission
-		foreach($fieldsToWrite as $field) {
-			$val = CapturedField::create();
-			$val->SubmissionID = $submission->ID;
+        // For every wanted field create a Captured Field object and write it to this submission
+        foreach($fieldsToWrite as $field) {
 
-			$field->performReadonlyTransformation();
-			$val->Name = $field->Name;
-			$val->Title = $field->Title() ?: $field->Name;
-			$val->IsInDetails = in_array($field->Name, $inDetails) ? '1' : '0';
+            $showInDetails = in_array($field->Name, $inDetails) ? '1' : '0';
 
-			// Add to this statement if any future type-based value conversions are required
-			switch ($field->Type()) {
-				case 'checkbox':
+            $capturedField = $this->create_captured_field($field, $showInDetails);
 
-						$val->Value = $field->dataValue() === 1 ? 'Yes' : 'No';
+            $capturedField->SubmissionID = $submission->ID;
 
-					break;
-				case 'groupeddropdown dropdown':
+            $capturedField->write();
 
-					// Relevent values
-					$groupedSrc = $field->getSourceAsArray();
-					$selected = $field->dataValue();
+        }
 
-					// Loop through all source keys, if we find an array search it for the field value
-					foreach ($groupedSrc as $key => $option) {
+        // Return an ID for this submission
+        return [
+            'ID' => $submission->ID,
+            'Link' => $this->get_submission_link($submission->ID)
+        ];
+    }
 
-						if(is_array($option) && array_search($selected, $option)) {
+    /**
+    * Method what returns a captured field constructed from
+    * a given value
+    *
+    * @param FormField $field The field to transform and write to the db
+    * @param boolean $showIndetails Controls whether the current field should show in the submission 'Details'
+    *
+    * @return CapturedField The final field for the submission
+    */
+    private function create_captured_field($field, $showInDetails = false)
+    {
+        $val = CapturedField::create();
 
-						// If there's a match return the key holding the value
-						$catForVal = $key;
+        $field->performReadonlyTransformation();
+        $val->Name = $field->Name;
+        $val->Title = $field->Title() ?: $field->Name;
+        $val->IsInDetails = $showInDetails;
 
-						}
-					}
+        // Add to this statement if any future type-based value conversions are required
+        switch ($field->Type()) {
+            case 'checkbox':
 
-					// Formatted value for CMS Display
-					$val->Value = $catForVal ? '[' . $catForVal .'] ' . $selected : $selected;
+                $val->Value = $field->dataValue() === 1 ? 'Yes' : 'No';
 
-					break;
-				default:
+                break;
+            case 'groupeddropdown dropdown':
 
-					$val->Value = $field->dataValue();
+                // Relevent values
+                $groupedSrc = $field->getSourceAsArray();
+                $selected = $field->dataValue();
 
-					break;
-			}
+                // Loop through all source keys, if we find an array search it for the field value
+                foreach ($groupedSrc as $key => $option) {
 
-			$val->write();
-		}
-	}
+                    if(is_array($option) && array_search($selected, $option)) {
+
+                        // If there's a match return the key holding the value
+                        $catForVal = $key;
+
+                    }
+                }
+
+                // Formatted value for CMS Display
+                $val->Value = $catForVal ? '[' . $catForVal .'] ' . $selected : $selected;
+
+                break;
+            default:
+
+                $val->Value = $field->dataValue();
+
+                break;
+        }
+
+        return $val;
+    }
+
+    /**
+     * Return a link which can be used externally for linking
+     * to a specific submission object in the CMS
+     *
+     * @param int $id The ID of the linked submission
+     *
+     * @return string
+     */
+    private function get_submission_link($id)
+    {
+        $base = Director::AbsoluteBaseURL() . singleton('FormCaptureAdmin')->Link();
+        $editorLink = $base . 'CapturedFormSubmission/EditForm/field/CapturedFormSubmission/item/';
+        return $editorLink . $id;
+    }
 }
