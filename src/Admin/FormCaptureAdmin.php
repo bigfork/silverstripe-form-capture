79e4d596e8c46b6bd1a0d4cbaf3386b74652e066
Kris <kriscoidan@bigfork.co.uk>
2023-01-30 09:12:34 +0000

    Updates methods to use beforeExtending instead of extend so that it is not called twice.

--- src/Admin/FormCaptureAdmin.php
+++ src/Admin/FormCaptureAdmin.php
@@ -39,94 +39,93 @@ class FormCaptureAdmin extends ModelAdmin
 
     public function getGridField(): GridField
     {
-        $field = parent::getGridField();
-        $field->setName('Submissions');
+        $this->beforeExtending('updateGridField', function (GridField $field) {
+            $field->setName('Submissions');
+        });
 
-        $this->extend('updateGridField', $field);
-
-        return $field;
+        return parent::getGridField();
     }
 
     public function getGridFieldConfig(): GridFieldConfig
     {
-        $config = parent::getGridFieldConfig();
-        $config->removeComponentsByType(GridFieldImportButton::class);
-        $config->removeComponentsByType(GridFieldPrintButton::class);
+        $this->beforeExtending('updateGridFieldConfig', function (GridFieldConfig $config) {
+            $config->removeComponentsByType(GridFieldImportButton::class);
+            $config->removeComponentsByType(GridFieldPrintButton::class);
 
-        // Configure filtering options
-        $updateSearchContext = function(SearchContext $context) {
-            $filters = $context->getFilters();
-            $filters['Name'] = HavingPartialMatchFilter::create('NameWithFallback');
-            $filters['Email'] = HavingPartialMatchFilter::create('EmailWithFallback');
-            $filters['MinDate'] = GreaterThanOrEqualFilter::create('Created');
-            $filters['MaxDate'] = LessThanOrEqualFilter::create('Created');
-            $context->setFilters($filters);
+            // Configure filtering options
+            $updateSearchContext = function (SearchContext $context) {
+                $filters = $context->getFilters();
+                $filters['Name'] = HavingPartialMatchFilter::create('NameWithFallback');
+                $filters['Email'] = HavingPartialMatchFilter::create('EmailWithFallback');
+                $filters['MinDate'] = GreaterThanOrEqualFilter::create('Created');
+                $filters['MaxDate'] = LessThanOrEqualFilter::create('Created');
+                $context->setFilters($filters);
 
-            $this->extend('updateSearchContext', $context);
-        };
+                $this->extend('updateSearchContext', $context);
+            };
 
-        $updateSearchForm = function(Form $form) {
-            $form->Fields()->insertBefore('Search__Type', $name = TextField::create('Name', 'Name'));
-            $form->Fields()->insertBefore('Search__Type', $email = TextField::create('Email', 'Email'));
+            $updateSearchForm = function (Form $form) {
+                $form->Fields()->insertBefore('Search__Type', $name = TextField::create('Name', 'Name'));
+                $form->Fields()->insertBefore('Search__Type', $email = TextField::create('Email', 'Email'));
 
-            // Replace free-text with dropdown of available submission types
-            $types = ArrayLib::valuekey(CapturedFormSubmission::get()->columnUnique('Type'));
-            $typeField = DropdownField::create('Search__Type', 'Type', $types)
-                ->setEmptyString(_t('SilverStripe\\Forms\\DropdownField.CHOOSE', '(Choose)'))
-                ->addExtraClass('stacked');
+                // Replace free-text with dropdown of available submission types
+                $types = ArrayLib::valuekey(CapturedFormSubmission::get()->columnUnique('Type'));
+                $typeField = DropdownField::create('Search__Type', 'Type', $types)
+                    ->setEmptyString(_t('SilverStripe\\Forms\\DropdownField.CHOOSE', '(Choose)'))
+                    ->addExtraClass('stacked');
 
-            $form->Fields()->replaceField('Search__Type', $typeField);
-            $form->Fields()->push($minDate = DateField::create('MinDate', 'Submitted from'));
-            $form->Fields()->push($maxDate = DateField::create('MaxDate', 'Submitted to'));
+                $form->Fields()->replaceField('Search__Type', $typeField);
+                $form->Fields()->push($minDate = DateField::create('MinDate', 'Submitted from'));
+                $form->Fields()->push($maxDate = DateField::create('MaxDate', 'Submitted to'));
 
-            foreach ([$name, $email, $minDate, $maxDate, $typeField] as $field) {
-                $field->addExtraClass('stacked')
-                    ->setForm($form);
+                foreach ([$name, $email, $minDate, $maxDate, $typeField] as $field) {
+                    $field->addExtraClass('stacked')
+                        ->setForm($form);
+                }
+
+                // Remove scaffolded fields
+                $form->Fields()->removeByName('Search__NameWithFallback');
+                $form->Fields()->removeByName('Search__EmailWithFallback');
+
+                $this->extend('updateSearchForm', $form);
+            };
+
+            $config->removeComponentsByType(GridFieldFilterHeader::class);
+            $config->addComponent(new GridFieldFilterHeader(false, $updateSearchContext, $updateSearchForm));
+            // Filter only updates page indicators if paginator is added *after* it, so we have to remove and re-add it
+            // See https://github.com/silverstripe/silverstripe-framework/issues/8454
+            $config->removeComponentsByType(GridFieldPaginator::class);
+            $config->addComponent(new GridFieldPaginator());
+
+            // Configure CSV export columns
+            $fieldTable = DataObject::getSchema()->tableName(CapturedField::class);
+            $columnSelect = SQLSelect::create()
+                ->setSelect('Name')
+                ->setFrom($fieldTable)
+                ->setGroupBy(['Name']);
+
+            $columns = [
+                'Type'    => 'Type',
+                'Created' => 'Date'
+            ];
+
+            foreach ($columnSelect->execute() as $row) {
+                $columns["export__{$row['Name']}"] = $row['Name'];
             }
 
-            // Remove scaffolded fields
-            $form->Fields()->removeByName('Search__NameWithFallback');
-            $form->Fields()->removeByName('Search__EmailWithFallback');
+            // Replace existing export button with new one (queued export if module is installed)
+            $config->removeComponentsByType(GridFieldExportButton::class);
+            if (class_exists(GridFieldQueuedExportButton::class)) {
+                $export = new GridFieldQueuedExportButton('buttons-before-left');
+            } else {
+                $export = new GridFieldExportButton('buttons-before-left');
+            }
 
-            $this->extend('updateSearchForm', $form);
-        };
+            $config->addComponent($export);
+            $export->setExportColumns($columns);
 
-        $config->removeComponentsByType(GridFieldFilterHeader::class);
-        $config->addComponent(new GridFieldFilterHeader(false, $updateSearchContext, $updateSearchForm));
-        // Filter only updates page indicators if paginator is added *after* it, so we have to remove and re-add it
-        // See https://github.com/silverstripe/silverstripe-framework/issues/8454
-        $config->removeComponentsByType(GridFieldPaginator::class);
-        $config->addComponent(new GridFieldPaginator());
+        });
 
-        // Configure CSV export columns
-        $fieldTable = DataObject::getSchema()->tableName(CapturedField::class);
-        $columnSelect = SQLSelect::create()
-            ->setSelect('Name')
-            ->setFrom($fieldTable)
-            ->setGroupBy(['Name']);
-
-        $columns = [
-            'Type' => 'Type',
-            'Created' => 'Date'
-        ];
-
-        foreach ($columnSelect->execute() as $row) {
-            $columns["export__{$row['Name']}"] = $row['Name'];
-        }
-
-        // Replace existing export button with new one (queued export if module is installed)
-        $config->removeComponentsByType(GridFieldExportButton::class);
-        if (class_exists(GridFieldQueuedExportButton::class)) {
-            $export = new GridFieldQueuedExportButton('buttons-before-left');
-        } else {
-            $export = new GridFieldExportButton('buttons-before-left');
-        }
-
-        $config->addComponent($export);
-        $export->setExportColumns($columns);
-
-        $this->extend('updateGridFieldConfig', $config);
-
-        return $config;
+        return parent::getGridFieldConfig();
     }
 }
